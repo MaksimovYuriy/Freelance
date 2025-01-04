77cb1094ab532c8312b50c36d2587432b2daa9f3
Анастасия Трушева <a.a.trusheva@stud.mospolytech.ru>
2025-01-05 02:22:05 +0300

    -FinishTask сообщения

---
+++ FreelanceDB/FreelanceDB/RabbitMQ/Contracts/FinishTaskMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace FreelanceDB.RabbitMQ.Contracts
+{
+    public record FinishTaskMessage
+    (
+        long AuthorId,
+        decimal Price,
+        long WorkerId
+    );
+}
--- FreelanceDB/FreelanceDB/RabbitMQ/RabbitMqService.cs
+++ FreelanceDB/FreelanceDB/RabbitMQ/RabbitMqService.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using System.Text;
 using Newtonsoft.Json;
+using FreelanceDB.RabbitMQ.Contracts;
 
 namespace FreelanceDB.RabbitMQ
 {
@@ -32,7 +33,6 @@ namespace FreelanceDB.RabbitMQ
             await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "CreateUserQueue", body: body);
             Console.WriteLine($" [x] Sent {message}");
 
-            Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
 
         }
@@ -52,12 +52,30 @@ namespace FreelanceDB.RabbitMQ
             await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "CreateTaskQueue", body: bytes);
             Console.WriteLine($" [x] Sent {message}");
 
-            Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
 
         }
 
-        
+        public async void SendFinishTaskMessage(long authorId, decimal price, long workerId)
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri(_config["RabbitMQ: ConnectionString"]) };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "FinishTaskQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var message = new FinishTaskMessage(authorId, price, workerId);
+            var body = JsonConvert.SerializeObject(message);
+            var bytes = Encoding.UTF8.GetBytes(body);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "FinishTaskQueue", body: bytes);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.ReadLine();
+
+        }
+
+
 
     }
 }
